2ae27524a2b6d55add4610e931ea3d892960d0c6
Avinash <avinashkmg1310@gmail.com>
2024-09-18 07:42:05 +0530

    simplified cart-reducer

--- src/store/cart-reducer.js
+++ src/store/cart-reducer.js
@@ -16,39 +16,56 @@ const cartSlice = createSlice({
 				(item) => item.id === newItem.id
 			);
 			if (!existingItem) {
-				state.cartList = [
-					...state.cartList,
-					{ ...newItem, quantity: 1, totalPrice: +newItem.price },
-				];
-			} else {
-				state.cartList = state.cartList.map((item) => {
-					return item.id === newItem.id
-						? {
-								...item,
-								quantity: item.quantity + 1,
-								totalPrice:
-									+newItem.price * (item.quantity + 1),
-						  }
-						: item;
+				state.cartList.push({
+					...newItem,
+					quantity: 1,
+					totalPrice: +newItem.price,
 				});
+				// state.cartList = [
+				// 	...state.cartList,
+				// 	{ ...newItem, quantity: 1, totalPrice: +newItem.price },
+				// ];
+			} else {
+				existingItem.quantity++;
+				existingItem.totalPrice += existingItem.price;
+				// state.cartList = state.cartList.map((item) => {
+				// 	return item.id === newItem.id
+				// 		? {
+				// 				...item,
+				// 				quantity: item.quantity + 1,
+				// 				totalPrice:
+				// 					+newItem.price * (item.quantity + 1),
+				// 		  }
+				// 		: item;
+				// });
 			}
 		},
 		removeFormCart: (state, actions) => {
 			state.totalItems--;
 			const id = actions.payload;
-			state.cartList = state.cartList
-				.map((item) => {
-					return item.id === id && item.quantity >= 1
-						? {
-								...item,
-								quantity: item.quantity - 1,
-								totalPrice: item.totalPrice - item.price,
-						  }
-						: item;
-				})
-				.filter((item) => {
-					return !(item.quantity === 0 && item.id === id);
+			const removeItem = state.cartList.find((item) => item.id === id);
+			if (removeItem.quantity > 1) {
+				removeItem.quantity--;
+				removeItem.totalPrice -= removeItem.price;
+			} else {
+				removeItem.totalPrice -= removeItem.price;
+				state.cartList = state.cartList.filter((item) => {
+					return !(item.id === id);
 				});
+			}
+			// state.cartList = state.cartList
+			// 	.map((item) => {
+			// 		return item.id === id && item.quantity >= 1
+			// 			? {
+			// 					...item,
+			// 					quantity: item.quantity - 1,
+			// 					totalPrice: item.totalPrice - item.price,
+			// 			  }
+			// 			: item;
+			// 	})
+			// 	.filter((item) => {
+			// 		return !(item.quantity === 0 && item.id === id);
+			// 	});
 		},
 	},
 });
